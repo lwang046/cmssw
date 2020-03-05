15eb11d3f15127e2d49d7c3ba955e068c027b67c
Long <long.wang@cern.ch>
2020-03-05 16:20:56 +0100

    test timing bit

--- SimCalorimetry/HcalTrigPrimAlgos/src/HcalTimingBit.cc
+++ SimCalorimetry/HcalTrigPrimAlgos/src/HcalTimingBit.cc
@@ -2,9 +2,22 @@
 
 #include <cassert>
 
-std::bitset<4> HcalTimingBit::compute(int ibin, HcalUpgradeTriggerPrimitiveDigi& TPDigi) const
+std::bitset<4> HcalTimingBit::compute(int ibin, HcalUpgradeTriggerPrimitiveDigi& digi) const
 {
       std::bitset<4> result;
+
+      std::vector<int> energy_depth = digi.getDepthData();
+
+      for (int i = 0; i < static_cast<int>(energy_depth.size()); ++i) {
+        
+
+
+      }
+
+      result[0] = 1;
+      result[1] = 0;
+      result[2] = 1;
+      result[3] = 0;
 
       return result;
 }
